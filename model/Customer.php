5a264132a6294827ece9072d28bc541ea593fbe7
namnguyen <toilanam97@gmail.com>
2019-09-23 13:51:01 +0700

    mvc

---
+++ model/Customer.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace model;
+class Customer
+{
+
+public $name;
+public $email;
+public $address;
+
+public function __construct($name,$email,$address)
+{
+    $this->name= $name;
+    $this->email= $email;
+    $this->address= $address;
+}
+
+}
